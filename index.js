d7ef548d0b4a4e4fbf49a2c9ee3c528fb81ab5e9
Your Name <briancasillas67@yahoo.com>
2022-02-22 17:21:42 -0800

    add prompts emails/github/index.js

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
-
 const inquirer = require("inquirer");
 const fs = require("fs");
-
-
+const generateMarkdown = require("./utils/generateMarkdown.js");
+// TODO: Create an array of questions for user input
 const questions = [];
-
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
+function init() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub username.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your email.");
+                    return false;
+                }
+            }
+        }
+    ]);
+};
 
 // Function call to initialize app
 init();
+
+console.log(process.argv[2]); 
+init().then(data => {
+    console.log(data);
+}); 
